6198e3f45014b0678a0df713faec050593fdcf5e
xennia87 <62959606+xennia87@users.noreply.github.com>
2023-03-01 21:45:55 +0100

    Calculos funcionando

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
+// Variables
+
+let firstNumber;
+let secondNumber;
+let operatorCalc = '';
+let result;
+
+const displayNumber = document.querySelector('.displayNumber');
+const displayOperator = document.querySelector('.displayOperator');
+
+// Mathematical functions
+
+function doMath(a, symbol) {
+    secondNumber = displayNumber.textContent;
+    displayNumber.textContent = '';
+    displayOperator.textContent += ` ${+secondNumber}`;
+    if (symbol == '+') {
+        result = add(+a, +secondNumber);
+    } else if (symbol == '-') {
+        result = subtract(+a, +secondNumber);
+    } else if (symbol == '*') {
+        result = multiply(+a, +secondNumber);
+    } else if (symbol == '/') {
+        result = divide(+a, +secondNumber);
+    }
+    displayNumber.textContent = result;
+}
+
 function add (a, b) {
     return a + b;
 };
@@ -14,25 +42,48 @@ function divide (a, b) {
     return a / b;
 };
 
-function operate(operator, a, b) {
-    if (operator == '+') {
-        return add(a, b);
-    } else if (operator == '-') {
-        return subtract(a, b);
-    } else if (operator == '*') {
-        return multiply(a, b) 
-    } else if (operator == '/') {
-        return divide(a, b);
-    }
-};
 
-const displayNumber = document.querySelector('.displayNumber');
+// Esta funcion muestra el número en pantalla
+
+function addNumber(number) {
+    displayNumber.textContent += number;
+}
+
+// Esta función guarda el primer número que se introduce
+
+function saveNumber(operator) {
+    firstNumber = displayNumber.textContent;
+    operatorCalc = operator;
+    displayNumber.textContent = '';
+    displayOperator.textContent += `${firstNumber} ${operator}`;
+    return firstNumber, operatorCalc;
+}
+
+// Esta función limpia el display 
+
+function clearAll() {
+    firstNumber = '';
+    operatorCalc = ''
+    secondNumber = ''
+    displayNumber.textContent = '';
+    displayOperator.textContent = '';
+}
 
 const buttons = document.querySelectorAll('button');
 buttons.forEach((button) => {
-    button.addEventListener('click', () => addNumber(button.textContent));
+    if (button.className == 'number') {
+        button.addEventListener('click', () => addNumber(button.textContent));
+    } else if (button.className == 'operator' && operatorCalc == '') { // NO FUNCIONA y operatorCalc está vacio porque es la primera vez que se pulsa
+        button.addEventListener('click', () => saveNumber(button.textContent));
+    } else if (button.className == 'operator' && operatorCalc != '') { // NO FUNCIONA o el className es operator y el operatorCalc existe 
+        button.addEventListener('click', () => doMath(firstNumber, operatorCalc));
+    } else if (button.className == 'equals') { 
+        button.addEventListener('click', () => doMath(firstNumber, operatorCalc));
+    } else if (button.className == 'clear') {
+        button.addEventListener('click', () => clearAll());
+    }
 });
 
-function addNumber(number) {
-    displayNumber.textContent += number
-}
+// Hacer que si el número es muy largo el tamaño del texto se ajuste.
+// Hacer que se puedan encadenar varias operaciones, 
+//    osease que el operator haga la cuenta igual que el equal si no es el primero 
